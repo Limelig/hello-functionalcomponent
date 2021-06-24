920da3dce2156596ae7d0311c63e280da77cc33a
Limelig <lowertps@naver.com>
2021-06-24 17:14:33 +0900

    react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">홈페이지</Link>
+          </li>
+          <li>
+            <Link to="/helloClass">클래스 컴포넌트 테스트</Link>
+          </li>
+          <li>
+            <Link to="/hellofunctional">함수형 컴포넌트 테스트</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <Switch>
+          <Route path="/" exact={true}>
+            홈페이지ㅣ
+          </Route>
+          <Route path="/helloClass" exact={true}>
+            클래스 컴포넌트 테스트
+          </Route>          
+          <Route path="/hellofunctional" exact={true}>
+            함수형 컴포넌트 테스트
+          </Route>
+        </Switch>
+      </div>
+      </Router>
   );
 }
 
